e22a64af48d582026e70153ca96ca9f9ba596dad
Yuka Ajisaka <47623066+osakana9114@users.noreply.github.com>
2023-05-05 17:23:51 +0900

    upgrade: AWS SDK, Lambdaランタイム

--- lib/sheldon-bot-3-stack.ts
+++ lib/sheldon-bot-3-stack.ts
@@ -1,38 +1,39 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { Architecture, FunctionUrlAuthType, Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import { ManagedPolicy } from '@aws-cdk/aws-iam'
+import {Construct} from 'constructs';
+import {Architecture, FunctionUrlAuthType, Runtime} from 'aws-cdk-lib/aws-lambda';
+import {NodejsFunction} from 'aws-cdk-lib/aws-lambda-nodejs';
+import {ManagedPolicy} from '@aws-cdk/aws-iam'
 
 export class SheldonBot3Stack extends cdk.Stack {
-    accountId: string
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-        super(scope, id, props);
+  accountId: string
 
-        // AWSアカウントID
-        this.accountId = cdk.Stack.of(this).account;
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
 
-        const botFunction = new NodejsFunction(this, 'main', {
-            runtime: Runtime.NODEJS_16_X,
-            architecture: Architecture.ARM_64,
-            entry: './src/index.ts',
-            handler: 'handler',
-            timeout: cdk.Duration.seconds(30),
-            memorySize: 256,
-            environment: {
-                'GUILD_ID': '',
-                'DISCORD_TOKEN': '',
-                'PUBLIC_KEY_PARAM_NAME': '/sheldon-bot/public-key',
-                'APP_ID_PARAM_NAME': '/sheldon-bot/app-id'
-            }
-        });
-        // 実行ロール
-        botFunction.role?.addManagedPolicy(
-            ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMReadOnlyAccess')
-        );
-        // 関数URL
-        botFunction.addFunctionUrl({
-            authType: FunctionUrlAuthType.NONE,
-        });
-    }
+    // AWSアカウントID
+    this.accountId = cdk.Stack.of(this).account;
+
+    const botFunction = new NodejsFunction(this, 'main', {
+      runtime: Runtime.NODEJS_18_X,
+      architecture: Architecture.ARM_64,
+      entry: './src/index.ts',
+      handler: 'handler',
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 256,
+      environment: {
+        'GUILD_ID': '',
+        'DISCORD_TOKEN': '',
+        'PUBLIC_KEY_PARAM_NAME': '/sheldon-bot/public-key',
+        'APP_ID_PARAM_NAME': '/sheldon-bot/app-id'
+      }
+    });
+    // 実行ロール
+    botFunction.role?.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMReadOnlyAccess')
+    );
+    // 関数URL
+    botFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,51 @@
-import {SSM} from 'aws-sdk';
-import {APIGatewayProxyEventV2, APIGatewayProxyResultV2} from "aws-lambda";
+import {SSMClient, GetParameterCommand} from '@aws-sdk/client-ssm';
+import {APIGatewayProxyEventV2, APIGatewayProxyResultV2} from 'aws-lambda';
 import * as Discord from 'discord-interactions';
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
-    const awsSSM = new SSM();
-    const publicKey = await awsSSM.getParameter({
-        Name: process.env.PUBLIC_KEY_PARAM_NAME,
-        WithDecryption: true
-    }).promise();
+  const client = new SSMClient({region: 'ap-northeast-1'});
+  const command = new GetParameterCommand({
+    Name: process.env.PUBLIC_KEY_PARAM_NAME,
+    WithDecryption: true,
+  });
+  const publicKey = await client.send(command);
 
-    // 署名の検証
-    const signature = event.headers['x-signature-ed25519'];
-    const timestamp = event.headers['x-signature-timestamp'];
-    const isValidRequest = Discord.verifyKey(event.body, signature as String, timestamp as String, publicKey.Parameter?.Value as String);
-    console.info('isValidRequest:', isValidRequest);
+  // 署名の検証
+  const signature = event.headers['x-signature-ed25519'];
+  const timestamp = event.headers['x-signature-timestamp'];
+  const isValidRequest = Discord.verifyKey(event.body, signature as String, timestamp as String, publicKey.Parameter?.Value as String);
+  console.info('isValidRequest:', isValidRequest);
 
-    if (!isValidRequest) {
-        return {
-            statusCode: 401,
-            body: JSON.stringify({
-                message: "Bad request signature"
-            })
-        };
-    }
+  if (!isValidRequest) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({
+        message: "Bad request signature"
+      })
+    };
+  }
 
-    const requestBody = JSON.parse(event.body);
-    const interactionType = requestBody.type;
-    console.info('Interaction type:', interactionType);
+  const requestBody = JSON.parse(event.body);
+  const interactionType = requestBody.type;
+  console.info('Interaction type:', interactionType);
 
-    if (interactionType === Discord.InteractionType.PING) {
-        // ACK
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                type: Discord.InteractionResponseType.PONG
-            })
-        };
-    } else if (interactionType === Discord.InteractionType.APPLICATION_COMMAND) {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                type: Discord.InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-                data: {
-                    content: 'スプラシューターを使うでし！'
-                }
-            })
-        };
-    }
+  if (interactionType === Discord.InteractionType.PING) {
+    // ACK
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: Discord.InteractionResponseType.PONG
+      })
+    };
+  } else if (interactionType === Discord.InteractionType.APPLICATION_COMMAND) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        type: Discord.InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: 'スプラシューターを使うでし！'
+        }
+      })
+    };
+  }
 };
